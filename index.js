a5e2fed525faa282793ece1f18017c6082ea9318
Francesco Negri <francesconegri@gmail.com>
2014-09-18 22:59:07 +0100

    node.js compatibility

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-(function () {alert('Will Scotland become independent?: ' + (Math.round(Math.random()) === 1 ? 'yes' : 'no'));}());
+(function () {
+    var logic = 'Will Scotland become independent?: ' + (Math.round(Math.random()) === 1 ? 'yes' : 'no');
+    if (typeof alert === 'function') alert(logic);
+    else console.log(logic);
+})();
